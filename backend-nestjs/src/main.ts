01ebd54f4fa5ef81b1e956126ed54c74824bd2dd
Anju Lubis <anjuucoklubis@gmail.com>
2024-08-06 21:29:34 +0700

    backend : set limit body parser json & urlencoded

--- backend-nestjs/src/main.ts
+++ backend-nestjs/src/main.ts
@@ -1,5 +1,6 @@
 import * as path from 'path';
 import * as dotenv from 'dotenv';
+import * as bodyParser from 'body-parser';
 import * as cookieParser from 'cookie-parser';
 import { AppModule } from './app.module';
 import { NestFactory } from '@nestjs/core';
@@ -10,10 +11,18 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 async function bootstrap() {
   dotenv.config();
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
-
   // app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   app.useGlobalPipes(new ValidationPipe());
   app.use(cookieParser());
+  app.use(bodyParser.json({ limit: '100mb' })); // Atur limit sesuai kebutuhan
+  app.use(
+    bodyParser.urlencoded({
+      limit: '100mb',
+      extended: true,
+      parameterLimit: 100000,
+    }),
+  );
+  app.use(bodyParser.text({ limit: '200mb' }));
   app.enableCors({
     origin: 'http://localhost:3001',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
